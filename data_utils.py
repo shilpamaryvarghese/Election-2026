21907064de7014c3fb29df74098f2f6674514faa
Salvin Sebastian <salvinsebastian308@gmail.com>
2026-05-04 09:49:50 +0530

    Update data_utils.py

--- data_utils.py
+++ data_utils.py
@@ -2,8 +2,6 @@ import pandas as pd
 
 def process_data(df):
     df["Votes"] = pd.to_numeric(df["Votes"], errors="coerce").fillna(0)
-
     winners = df.loc[df.groupby("Constituency")["Votes"].idxmax()]
     seat_count = winners["Party"].value_counts()
-
     return df, winners, seat_count
