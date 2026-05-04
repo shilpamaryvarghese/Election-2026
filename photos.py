5e4e82702a86b6391e6be21d3e57d0730e611707
Salvin Sebastian <salvinsebastian308@gmail.com>
2026-05-04 09:50:04 +0530

    Update photos.py

--- photos.py
+++ photos.py
@@ -4,11 +4,8 @@ def get_candidate_photo(name):
     try:
         url = f"https://en.wikipedia.org/api/rest_v1/page/summary/{name}"
         res = requests.get(url).json()
-
         if "thumbnail" in res:
             return res["thumbnail"]["source"]
-
     except:
         pass
-
     return "https://via.placeholder.com/100"
